24353d5c570dd2e0dc523813434b918d4d1d9d04
Raymond Hernandez <67213974+rayherna18@users.noreply.github.com>
2023-03-24 12:30:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,22 @@
     <title>Homepage</title>
 </head>
 <body>
-        <main class="bg">
-            
-        </main>
+    <main class="bg">
+        <form action="/action_page.php" method="post">
+
+            <div class="loginBox">
+                <img src="./logo.gif" alt="logo" id="logo">
+                <label for="name" class="credentials"><b>Username</b></label>
+                <input type="text" placeholder="Username" name="name" required ><br>
+
+                <label for="pass"><b>Password</b></label>
+                <input type="password" placeholder="Password" name="pass" required><br>
+        
+                <button type="submit">Login</button>
+                <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+            </div>
+    </main>
 </body>
 </html>
